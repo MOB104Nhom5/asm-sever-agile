a8d622516f2a2cf8138df628826f6ca445d22829
tho <mob104nhom5@gmail.com>
2022-06-15 16:15:59 +0700

    15/6

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var AuthMiddleware = require('../middleware/auth.midllware')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',AuthMiddleware.YeuCauDangNhap, function(req, res, next) {
   res.render('index');
 });
 router.post("/", (req, res) => {
